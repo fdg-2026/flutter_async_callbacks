9e33f1e90f86a43d9c61f047662a7eeeaed9280d
gs <gs@xyz.de>
2026-04-24 16:06:23 +0200

    main with Future.delayed in the callback

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: avoid_print
 import 'package:flutter/material.dart';
+import 'dart:io'; // needed for sleep
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Async tests',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Async Tests'),
     );
   }
 }
@@ -29,39 +31,69 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int? lastFound;
+  bool wasStarted = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Text("last found: $lastFound"),
+            ElevatedButton(
+              onPressed: wasStarted ? null : startCalculation,
+              child: const Text("Start calculation"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+
+  void startCalculation() async {
+    setState(() {
+      wasStarted = true;
+    });
+
+    // var countFound1 = performCalculation();
+    // countFount1 = 5;
+
+    int countFound = await performCalculation();
+    print("found $countFound prime numbers");
+    setState(() {
+      wasStarted = false;
+    });
+  }
+
+  Future<int> performCalculation() async {
+    var countFound = 0;
+    for (int i = 5; i < 5000; i += 2) {
+      if (isPrime(i)) {
+        print("found prime $i");
+        setState(() {
+          lastFound = i;
+        });
+        countFound++;
+        await Future.delayed(const Duration(milliseconds: 1));
+        // next line would be a synchronous wait (not available in Chrome)
+        //sleep(const Duration(milliseconds: 1));
+      }
+    }
+    return countFound;
+  }
+
+  bool isPrime(int test) {
+    for (int i = 3; i < test / 2; i += 2) {
+      if (test % i == 0) {
+        return false;
+      }
+    }
+    return true;
   }
 }
